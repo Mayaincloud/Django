4f7d002aada5890595decd1d5b09bbda28c783e8
Olga <89043833391@mail.ru>
2023-09-11 20:55:49 +0500

    Initial commit

---
+++ myproject/myapp2/management/commands/create_user.py
@@ -0,0 +1,12 @@
+from django.core.management.base import BaseCommand
+from myapp2.models import User
+
+
+class Command(BaseCommand):
+    help = "Create user."
+
+    def handle(self, *args, **kwargs):
+        user = User(name='Stan', email='stan@example.com',
+                    password='secret', age=43)
+        user.save()
+        self.stdout.write(f'{user}')
